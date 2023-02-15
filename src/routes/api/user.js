1ad2cac93492c30ec5dc1f7ce3ccd8c02f2be503
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-02-15 22:20:49 +0200

    add endpoint get current user

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,11 +1,12 @@
-const express = require('express');
-
-const {} = require('../../controllers/user.controller');
-const { tryCatchWrapper } = require('../../helpers');
-const { validateBody } = require('../../middlewares');
-
-const {} = require('../../schemas/user');
-
+const express = require("express");
 const router = express.Router();
+
+const { getCurrentUser } = require("../../controllers/user.controller");
+// const { tryCatchWrapper } = require('../../helpers');
+// const { validateBody } = require('../../middlewares');
+
+// const {} = require('../../schemas/user');
+
+router.get("/me", getCurrentUser);
 
 module.exports = router;
