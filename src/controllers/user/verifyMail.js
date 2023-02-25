a1db16df012dc31c050ee15777e3b26c609c4bfe
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2023-02-25 22:07:12 +0200

    VerifyMail-update

--- src/controllers/user/verifyMail.js
+++ src/controllers/user/verifyMail.js
@@ -1,26 +1,26 @@
 const { dbUsers } = require("../../models/user");
 const { HttpError } = require("../../helpers/httpError");
+const { sendConfirmMail } = require("../../helpers/sendVerifyMail");
 
 async function verifyMail(req, res, next) {
-	const { verificationToken } = req.params;
+    const { verificationToken } = req.params;
 
-	const user = await dbUsers.findOne({
-		verificationToken: verificationToken,
-	});
+    const user = await dbUsers.findOne({
+        verificationToken: verificationToken,
+    });
 
-	if (!user) {
-		throw new HttpError(404, error.message);
-	};
+    if (!user) {
+        throw new HttpError(404, error.message);
+    };
 
-	await dbUsers.findByIdAndUpdate(user._id, {
-		verifyEmail: true,
-		verificationToken: "",
-	});
-
-	// res.redirect(`${FRONTEND_URL}/api/users/login?token=${token}&email=${email}&id=${id}`);
-	return res.status(200).json({
-		message: "Verification successful",
-	});
+    await dbUsers.findByIdAndUpdate(user._id, {
+        verifyEmail: true,
+        verificationToken: "",
+    });
+    const { email, name } = user;
+    await sendConfirmMail(email, name);
+    // res.redirect(`${FRONTEND_URL}/api/users/login?token=${token}&email=${email}&id=${id}`);
+    return;
 }
 
 module.exports = verifyMail;
