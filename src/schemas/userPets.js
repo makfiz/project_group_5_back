3f656a063a014f977ffcd7c8a99af6cb2b3f4ff2
NataliaMartynuik <98057592+NataliaMartynuik@users.noreply.github.com>
2023-02-26 13:41:42 +0200

    fix validation

--- src/schemas/userPets.js
+++ src/schemas/userPets.js
@@ -1,17 +1,16 @@
 const Joi = require('joi');
-const dateBirthRegExp =
-  /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
-
+const dateBirthRegExp = /^(0[1-9]|[1-2][0-9]|3[0-1])\.(0[1-9]|1[0-2])\.[0-9]{4}$/;
+ 
 const createPetSchema = Joi.object({
   name: Joi.string()
-    .pattern(/^[a-zA-Z\s]+$/)
+    .pattern(/^[A-Za-zА-Яа-яІіЇїЄє'-.]+$/)
     .min(2)
     .max(16),
   dateOfBirth: Joi.string().pattern(dateBirthRegExp),
   breed: Joi.string()
-    .pattern(/^[a-zA-Z\s]+$/)
+    .pattern((/^[A-Za-zА-Яа-яІіЇїЄє'-.]+$/))
     .min(2)
-    .max(24),
+    .max(16),
   comments: Joi.string().min(8).max(120),
   petImage: null,
 });
